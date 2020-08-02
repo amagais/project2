35e73ede0c43c3f64c9dca021e7e4e02cfccde3b
amagais <60762975+amagais@users.noreply.github.com>
2020-08-01 23:51:39 -0500

    add

--- plotly2/static/js/app.js
+++ plotly2/static/js/app.js
@@ -20,9 +20,7 @@ function init() {
   });
   buildMetadata("AL");
   buildchart();
-  buildbubble();
-  buildbubble2();
-  
+
 };
 
 function buildMetadata(sampleID){
@@ -56,23 +54,15 @@ function optionChanged(sampleID){
   //buildbubble(sampleID);
 }
 
-var accessArray = []
-var dictArray = []
-var sortedaccessArray = []
-var sortedStateArray = []
-var snapArray = []
-var fastfoodArray = []
-var accessCountArray = []
+
 
 function buildchart(){
- 
+  var accessArray = []
+  var dictArray = []
+  var sortedaccessArray = []
+  var sortedStateArray = []
+
   d3.csv(csv).then((data) => {
-    
-    
-    data.forEach(function(d){
-      var access_data = d["Percent_Pop_Low_Access_to_Stores_2015"]
-      accessArray.push(access_data)
-    })
 
     for (var i = 0; i < stateArray.length; i++){
       var access_data = data[i]["Percent_Pop_Low_Access_to_Stores_2015"]
@@ -82,7 +72,6 @@ function buildchart(){
       var dict = {"state": curState, "low_access": access_data}
       dictArray.push(dict)
     }
-
 
     sortedaccessArray = accessArray.sort(function(a,b){return a-b});
     sortedaccessArray.forEach(function(a){
@@ -94,117 +83,31 @@ function buildchart(){
       }
     
     })
+    var ctx = document.getElementById('myChart');
+    myChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: { 
+            labels: sortedStateArray,
+            datasets: [{
+                label: 'Access to stores',
+                data: sortedaccessArray,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    
+});
 
-    //create trace
-    var trace = {
-      x: sortedaccessArray,
-      y: sortedStateArray,
-      text: sortedStateArray,
-      type: "bar",
-      orientation: "h",
-    }
-    //create data variable 
-    var data = [trace]
-
-    //create layout
-    var layout = {
-      margine:{
-        l: 100,
-        r: 100,
-        t: 100, 
-        b: 100
-      }
-    };
-
-    Plotly.newPlot("barchart",data, layout);
-  });
 };
-//Create the bubble chart, looking at the number of SNAP authorized stores in resepct to low food insecuirty population
-//The bigger the bubble, the more SNAP authorized stores 
-function buildbubble(){
-  d3.csv(csv).then((data) => {
-
-    data.forEach(function(d){
-      var access_count = d["Population_Low_Access_to_Stores_2015"]
-      accessCountArray.push(access_count)
-    })
-    
-    for (var i = 0; i < data.length; i++){
-      var SNAP_store_2016 = data[i].SNAP_Authorized_Stores_2016
-      snapArray.push(SNAP_store_2016)
-      
-    }  
-   
-  
-
-    var snapResizedArray = []
-    snapArray.forEach(function(snap){
-      newSize = snap/250
-      snapResizedArray.push(newSize)
-    })
-
-     var bubbleTrace = {
-       x: snapArray,
-       y: accessCountArray,
-       mode : "markers",
-       marker: {
-        size: snapResizedArray,
-        color: "purple",
-      },
-      text: stateArray
-     };
-
-     var data = [bubbleTrace];
-
-     var layout = {
-       showlegend: false,
-       height:600,
-       width: 600,
-     };
-    Plotly.newPlot("bubblechart",data,layout);
-    
-  })
-}
-
-//fast food restaurants in respect to low access population
-//the bigger the bubbles, the more fast food restaurants in the state
-function buildbubble2(){
-  d3.csv(csv).then((data) => {
-    for (var i = 0; i < data.length; i++){
-      fastfoodStore = data[i]["Fast-food_Restaurants_2014"]
-      fastfoodArray.push(fastfoodStore)
-      
-    }  
-    
-
-    var fastfoodResizedArray = []
-    fastfoodArray.forEach(function(f){
-      newSize2 = f/250
-      fastfoodResizedArray.push(newSize2)
-    })
-
-     var bubbleTrace2 = {
-       x: fastfoodArray,
-       y: accessCountArray,
-       mode : "markers",
-       marker: {
-        size: fastfoodResizedArray,
-        color: "yellow",
-      },
-      text: stateArray
-     };
-
-     var data = [bubbleTrace2];
-
-     var layout = {
-       showlegend: false,
-       height:600,
-       width: 600,
-     };
-    Plotly.newPlot("bubblechart2",data,layout);
-    
-  })
-}
 
 
 
